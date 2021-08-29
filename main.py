48087579a62e800e3f8c3d8b34dd1caf71fa5f7a
Derek Ratcliff <derek.ratcliff@gmail.com>
2021-08-29 14:09:28 -0400

    Convert guild id to string

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ dynamodb = boto3.resource("dynamodb", "us-east-2")
 def get_message(guild_id):
     table = dynamodb.Table('bot')
     response = table.query(
-        KeyConditionExpression=Key('id').eq(guild_id + ".msg"))
+        KeyConditionExpression=Key('id').eq(str(guild_id) + ".msg"))
     return response["Items"][0]["value"]
 
 
 def get_config(guild_id):
     table = dynamodb.Table('bot')
     response = table.query(
-        KeyConditionExpression=Key('id').eq(guild_id + ".yaml"))
+        KeyConditionExpression=Key('id').eq(str(guild_id) + ".yaml"))
     return yaml.safe_load(response["Items"][0]["value"])
 
 def get_token():
