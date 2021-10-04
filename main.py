53bbfdbc2f995cf8d522acc7163989ee1f0ae26e
Derek Ratcliff <derek.ratcliff@gmail.com>
2021-10-04 09:54:03 -0400

    Add logging

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import discord
-from discord import user
-from discord.ext import commands
+import logging
+import traceback
 from datetime import datetime
 from string import Template
-import traceback
+
 import boto3
-from boto3.dynamodb.conditions import Key
+import discord
 import yaml
+from boto3.dynamodb.conditions import Key
+from discord import user
+from discord.ext import commands
+
+logging.basicConfig(filename='bot.log',
+                    encoding='utf-8', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s - %(message)s')
 
 sun_emoji = '\U00002600'
 rain_emoji = '\U0001F327'
@@ -15,6 +21,7 @@ x_emoji = '\U0000274C'
 lock_emoji = '\U0001F512'
 
 dynamodb = boto3.resource("dynamodb", "us-east-2")
+
 
 def get_message(guild_id):
     table = dynamodb.Table('bot')
@@ -29,11 +36,13 @@ def get_config(guild_id):
         KeyConditionExpression=Key('id').eq(str(guild_id) + ".yaml"))
     return yaml.safe_load(response["Items"][0]["value"])
 
+
 def get_token():
     table = dynamodb.Table('bot')
     response = table.query(
         KeyConditionExpression=Key('id').eq("token"))
     return response["Items"][0]["value"]
+
 
 TOKEN = get_token()
 
@@ -49,6 +58,7 @@ bot = commands.Bot(command_prefix="moo.", intents=intents,
 properties = {}
 message_templates = {}
 
+
 def format_message(message, arg1, arg2, arg3):
     temp_obj = Template(message)
     return temp_obj.substitute(name=arg1, trial=arg2, no_show_ct=arg3)
@@ -56,7 +66,7 @@ def format_message(message, arg1, arg2, arg3):
 
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+    logging.info("We have logged in as {0.user}".format(client))
 
 
 @bot.command()
@@ -69,7 +79,9 @@ async def on_message(message):
     if message.author == bot.user or message.content.startswith("moo."):
         return
     elif "bruh" in str.lower(message.content):
+        logging.debug("bruh")
         await message.add_reaction('\N{THUMBS UP SIGN}')
+
 
 async def handle_confirmation_embed(reaction_payload, msg):
     e = msg.embeds[0]
@@ -84,7 +96,8 @@ async def handle_confirmation_embed(reaction_payload, msg):
     host = [x.value for x in e.fields if x.name == "Host"][0]
     event_channel = [x.value for x in e.fields if x.name == "Event Channel"][0]
     guild_id = [x.value for x in e.fields if x.name == "Guild ID"][0]
-    print("host", host, "cleared", cleared, "filled", filled, "event", event_channel, "guild id", guild_id)
+    logging.debug("host", host, "cleared", cleared, "filled", filled,
+          "event", event_channel, "guild id", guild_id)
     user = bot.get_user(reaction_payload.user_id)
     response_message = host + " hosted an event in " + event_channel + ". Filled: `" + \
         str(filled) + "`. Cleared: `" + str(cleared) + "`."
@@ -92,15 +105,17 @@ async def handle_confirmation_embed(reaction_payload, msg):
     channels = await guild.fetch_channels()
     guild_config = get_config(guild_id)["config"]
     run_log_channel_name = guild_config["run_log_channel"]["name"]
-    run_log_channel = [x for x in channels if x.name == run_log_channel_name][0]
+    run_log_channel = [
+        x for x in channels if x.name == run_log_channel_name][0]
     await run_log_channel.send(response_message)
+
 
 async def handle_reactions_to_bot_dm(reaction_payload, msg):
     if msg.embeds is not None and len(msg.embeds) == 1:
         e = msg.embeds[0]
         if e.title == "Confirmation" and reaction_payload.emoji.name == lock_emoji:
             await handle_confirmation_embed(reaction_payload, msg)
-            
+
 
 async def on_raw_reaction_add(reaction_payload):
     if reaction_payload.guild_id is None:
@@ -114,10 +129,11 @@ def author_role(ctx, guild_config):
     if "role" in guild_config:
         role_name = guild_config["role"]
         role = discord.utils.find(
-                lambda r: r.name == role_name, ctx.message.guild.roles)
+            lambda r: r.name == role_name, ctx.message.guild.roles)
     else:
         role = None
     return role
+
 
 @bot.command(help="Report <user> as a no-show for <event>. <count> is the current number of no-shows for this member.")
 async def noshow(ctx, user, event, count):
@@ -126,7 +142,7 @@ async def noshow(ctx, user, event, count):
         guild_config = get_config(guild_id)["config"]
         role = author_role(ctx, guild_config)
         if role is not None and role not in ctx.message.author.roles:
-            print("Not authorized")
+            logging.info("Not authorized: " + ctx.message.author.name)
             return
         id = ctx.message.mentions[0].id
         user = bot.get_user(id)
@@ -139,10 +155,11 @@ async def noshow(ctx, user, event, count):
         await channel.send("noshow for <@" + str(id) + "> reported in channel `" + ctx.message.channel.name + "` at " +
                            datetime.today().strftime('%Y-%m-%d-%H:%M:%S') + ". `count: " + str(count) + "`")
     except Exception as e:
-        traceback.print_exc(e)
+        logging.exception(e)
         user = bot.get_user(ctx.message.author.id)
         await user.send("I couldn't find a valid user for *" + user + "*. Make sure to mention (@) the user you're trying to noshow.")
     await ctx.message.delete()
+
 
 @bot.command(help="Give your core a name and it will automatically create the role and the Apply and the Core Channels for you.")
 async def createcore(ctx, corename):
@@ -150,26 +167,29 @@ async def createcore(ctx, corename):
     guild_config = get_config(guild.id)["config"]
     role = author_role(ctx, guild_config)
     if role is not None and role not in ctx.message.author.roles:
-        print("Not authorized")
+        logging.info("Not authorized: " + ctx.message.author.name)
         return
-    
+
     roles = guild.roles
     author = ctx.author
-    #print(ctx.author)
-    #Create new role.
-    trialsrole = [x for x in roles if x.name == "--------------❖  CORES  ❖-------------"][0]
-    #print(trialsrole.position)
+    logging.debug(ctx.author.name)
+    # Create new role.
+    trialsrole = [x for x in roles if x.name ==
+                  "--------------❖  CORES  ❖-------------"][0]
+    # print(trialsrole.position)
     newrole = await guild.create_role(name="Core - " + corename)
     await author.add_roles(newrole)
-    #print(newrole.name)
+    logging.debug(newrole.name)
     positions = {
         newrole: trialsrole.position-1
     }
     await guild.edit_role_positions(positions=positions)
-    #Create new channels for core.
+    # Create new channels for core.
     categories = guild.categories
-    coregroups = [x for x in categories if str.lower(x.name) == "core groups"][0]
-    opencores = [x for x in categories if str.lower(x.name) == "open core applications"][0]
+    coregroups = [x for x in categories if str.lower(
+        x.name) == "core groups"][0]
+    opencores = [x for x in categories if str.lower(
+        x.name) == "open core applications"][0]
     daedricprincerole = [x for x in roles if x.name == "Daedric Prince"][0]
     goldensaintsole = [x for x in roles if x.name == "Golden Saints"][0]
     overwrites = {
@@ -181,22 +201,25 @@ async def createcore(ctx, corename):
     await guild.create_text_channel(corename, overwrites=overwrites, category=coregroups)
     await guild.create_text_channel("Apply " + corename, overwrites=None, category=opencores)
 
+
 @bot.command(help="Create a log entry for a trial led by <raid_lead> listed in <event_channel>")
 async def archive(ctx, raid_lead, event_channel):
     try:
         guild_id = ctx.guild.id
-        # guild_config = get_config(guild_id)["config"]
-        # role = author_role(ctx, guild_config)
-        # if role is not None and role not in ctx.message.author.roles:
-        #     print("Not authorized")
-        #     return
+        guild_config = get_config(guild_id)["config"]
+        role = author_role(ctx, guild_config)
+        if role is not None and role not in ctx.message.author.roles:
+            logging.info("Not authorized: " + ctx.message.author.name)
+            return
         id = ctx.message.author.id
         user = bot.get_user(id)
         info_msg = "React with :sunny: if the raid took place or :cloud_rain: if it was cancelled.\n\nReact with :white_check_mark: if the raid cleared or :x: if it did not.\n\nWhen you are done, react with :lock: to post the log entry."
         message = raid_lead + " hosted an event in channel " + event_channel + "."
-        embedVar = discord.Embed(title="Confirmation", description="Please verify the following details to log this entry.", color=0x00ff00)
+        embedVar = discord.Embed(
+            title="Confirmation", description="Please verify the following details to log this entry.", color=0x00ff00)
         embedVar.add_field(name="Host", value=raid_lead, inline=False)
-        embedVar.add_field(name="Event Channel", value=ctx.message.channel_mentions[0].name, inline=False)
+        embedVar.add_field(
+            name="Event Channel", value=ctx.message.channel_mentions[0].name, inline=False)
         embedVar.add_field(name="Guild Name", value=ctx.guild, inline=False)
         embedVar.add_field(name="Guild ID", value=ctx.guild.id, inline=False)
         embedVar.add_field(name="Instructions", value=info_msg, inline=False)
@@ -208,11 +231,11 @@ async def archive(ctx, raid_lead, event_channel):
         await sent_message.add_reaction(lock_emoji)
 
     except Exception as e:
-        traceback.print_exc(e)
+        logging.exception(e)
         user = bot.get_user(ctx.message.author.id)
         await user.send("I couldn't find a valid user for *" + user + "*. Make sure to mention (@) the user you're trying to noshow.")
     await ctx.message.delete()
 
-bot.add_listener(on_message)    
+bot.add_listener(on_message)
 bot.add_listener(on_raw_reaction_add)
 bot.run(TOKEN)
