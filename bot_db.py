bd824124218ae040d7a532242f71b154df7add3d
mlance6210 <109550165+mlance6210@users.noreply.github.com>
2022-08-11 20:28:19 -0400

    Update bot_db.py

--- bot_db.py
+++ bot_db.py
@@ -26,14 +26,14 @@ dynamodb = dynamodb_client()
 
 
 def get_message(guild_id):
-    table = dynamodb.Table('bot')
+    table = dynamodb.Table('betabot')
     response = table.query(
         KeyConditionExpression=Key('id').eq(str(guild_id) + ".msg"))
     return response["Items"][0]["value"]
 
 
 def get_config(guild_id):
-    table = dynamodb.Table('bot')
+    table = dynamodb.Table('betabot')
     response = table.query(
         KeyConditionExpression=Key('id').eq(str(guild_id) + ".yaml"))
     return yaml.safe_load(response["Items"][0]["value"])
@@ -43,7 +43,7 @@ def get_token():
     if env == "local":
         with open("token_test.txt", "r") as f:
             return f.readline()
-    table = dynamodb.Table('bot')
+    table = dynamodb.Table('betabot')
     response = table.query(
         KeyConditionExpression=Key('id').eq("token"))
     return response["Items"][0]["value"]
