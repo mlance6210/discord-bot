8187dcb59db6bdd134fe23874b8191dd72f9c623
mlance6210 <109550165+mlance6210@users.noreply.github.com>
2022-08-13 20:35:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def on_message(message):
         logging.info("bruh")
         await message.add_reaction('\N{THUMBS UP SIGN}')
     elif "blamefey" in str.lower(message.content):
-        logging.info("bruh")
+        logging.info("blamefey")
         await message.add_reaction('\N{THUMBS UP SIGN}')
 
 
