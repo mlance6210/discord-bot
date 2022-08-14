069450915509ff75e714d549d7e0c379cd210281
mlance6210 <109550165+mlance6210@users.noreply.github.com>
2022-08-13 21:44:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def on_message(message):
         await message.add_reaction('\N{THUMBS UP SIGN}')
     elif "blamefey" in str.lower(message.content):
         logging.info("blamefey")
-        await message.add_reaction('\N{THUMBS UP SIGN}')
+        await message.add_reaction('<:feyistyping:916878041123287060>')
 
 async def handle_confirmation_embed(reaction_payload, msg):
     e = msg.embeds[0]
